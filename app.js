816a1938581da52af1e555df7e48e4cd4ae2495b
yue huanying <liuxing917067811@outlook.com>
2023-09-10 11:22:31 +0800

    1.1

--- app.js
+++ app.js
@@ -3,9 +3,97 @@ App({
     // 第一次打开
     // options.query == {number:1}
     console.info('App onLaunch');
+    this.getAuthCode(); //获取用户名
+    this.updateVersion(); // 检查更新
+
   },
   onShow(options) {
     // 从后台被 scheme 重新打开
     // options.query == {number:1}
+    this.getAuthCode(); //获取用户名
+    
+    this.updateVersion(); // 检查更新
   },
+  globalData: {
+    userName: "dd",
+    
+  },
+
+
+//获取用户名
+async getAuthCode (){
+  // console.log(this.globalData.userName);
+  //获取免登码
+  const res= await dd.getAuthCode();
+  // console.log(JSON.stringify(res.authCode));
+
+   //调用api
+  const res1 = await dd.httpRequest({
+    headers: {
+      "Content-Type": "application/json"
+    },
+    url: 'https://wlsl3q.forestpacking.com:4443/api/UserName',
+    method: 'POST',
+
+    // 需要手动调用JSON.stringify将数据进行序列化
+    data: JSON.stringify({
+        Code:  res.authCode,
+    }),
+    dataType: 'json',
+
+    success: function(res) {
+      
+      // console.log(JSON.parse(JSON.stringify(res.data)).name);
+      //获取用户名
+      // app.globalData.userName=JSON.parse(JSON.stringify(res.data)).name;
+      //  console.log(app.globalData.userName);
+
+    }, 
+    fail: function(res) {
+      dd.alert({content: JSON.stringify(res)});
+    },
+    complete: function(res) {
+      // console.log(authCode);
+           
+    }
+
+  });
+  
+  // console.log(res1.data.name);
+  this.globalData.userName = res1.data.name;
+
+  console.log( this.globalData.userName);
+
+ },
+
+
+  updateVersion(){
+
+    const updateManager = dd.getUpdateManager()
+  
+    updateManager.onCheckForUpdate(function (res) {
+      // 请求完新版本信息的回调
+      console.log(res.hasUpdate) // 是否有更新
+    })
+    
+    updateManager.onUpdateReady(function (ret) {
+      console.log(ret.version) // 更新版本号
+      dd.confirm({
+        title: '更新提示',
+        content: '新版本已经准备好，是否重启应用？',
+        success: function (res) {
+          if (res.confirm) {
+            // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+            updateManager.applyUpdate()
+          }
+        }
+      })
+    })
+    updateManager.onUpdateFailed(function () {
+      // 新版本下载失败
+    })
+  
+  }
+
+
 });
--- pages/coardboard/coardboard.js
+++ pages/coardboard/coardboard.js
@@ -1,3 +1,5 @@
+const app = getApp();
+
 Page({
   data: {
     objectArray: [
@@ -32,60 +34,61 @@ Page({
     crrcdeArray: [
       {
         id: 0,
-        name: 'BC'
+        name: 'BC',
       },
       {
         id: 1,
-        name: 'EB'
+        name: 'EB',
       },
       {
         id: 2,
-        name: 'EE'
+        name: 'EE',
       },
       {
         id: 3,
-        name: 'BA'
+        name: 'BA',
       },
       {
         id: 4,
-        name: 'B'
+        name: 'B',
       },
       {
         id: 5,
-        name: 'E'
+        name: 'E',
       },
       {
         id: 6,
-        name: 'C'
+        name: 'C',
       },
       {
         id: 7,
-        name: 'B'
+        name: 'A',
       },
       {
         id: 8,
-        name: 'A'
+        name: 'BCB',
       },
       {
         id: 9,
-        name: 'BCB'
+        name: 'BBC',
       },
       {
         id: 10,
-        name: 'BBC'
+        name: 'EBC',
       },
       {
         id: 11,
-        name: 'EBC'
-      },
-      {
-        id: 12,
-        name: 'EBA'
+        name: 'EBA',
       },
     ],
 
     arrIndex: 1, //厂区
-    crrIndex: 0, //瓦楞
+    crrIndex: 0,  //瓦楞
+    value_matcde: 'F314D',
+    value_lssrat: 1.05,//损耗
+    value_strprc: 0.45,//加工成本
+    value_inrate: 1.08, //利润
+    value_price: 0,//参考平方价
   },
   onLoad() {},
 
@@ -96,12 +99,182 @@ Page({
       arrIndex: e.detail.value,
     });
   },
-
+//瓦楞选择
   bindCrrPickerChange(e) {
+  
     console.log('picker发送选择改变，携带值为', e.detail.value);
     this.setData({
-      crrIndex: e.detail.value,
+      crrIndex: e.detail.value.toString().toUpperCase(), 
     });
+  
   },
+
+  //材质输入
+  handle_matcde(e){
+
+    let tmp = e.detail.value.toUpperCase();
+    console.log('材质输入：',e.detail.value.toUpperCase());
+    this.setData({
+      value_matcde: e.detail.value.toUpperCase(),
+    });
+    
+    if(tmp.length < 3){
+      this.setData({
+        crrcdeArray: [
+          {
+            id: 0,
+            name: 'B',
+          },
+          {
+            id: 1,
+            name: 'E',
+          },
+          {
+            id: 2,
+            name: 'C',
+          },
+          {
+            id: 3,
+            name: 'A',
+          }
+        ],
+        crrIndex: 0,
+      });
+    }else if(tmp.length > 5) {
+      this.setData({
+        crrcdeArray: [
+          {
+            id: 0,
+            name: 'BCB',
+          },
+          {
+            id: 1,
+            name: 'EBC',
+          },
+          {
+            id: 2,
+            name: 'BBC',
+          },
+          {
+            id: 3,
+            name: 'BBA',
+          },
+          {
+            id: 4,
+            name: 'EBA',
+          }
+        ],
+        crrIndex: 0,
+      });
+    }else {
+      this.setData({
+        crrcdeArray: [
+          {
+            id: 0,
+            name: 'BC',
+          },
+          {
+            id: 1,
+            name: 'EB',
+          },
+          {
+            id: 2,
+            name: 'EE',
+          },
+          {
+            id: 3,
+            name: 'BA',
+          },
+  
+        ],
+        crrIndex: 0,
+      });
+
+    }
+
+  },
+//损耗
+handle_lssrat(e){
+  console.log("损耗",e.detail.value);
+  this.setData({
+    value_lssrat: e.detail.value,
+  });
+},
+//加工成本
+handle_strprc(e){
+  console.log("加工成本：",e.detail.value);
+  this.setData({
+    value_strprc: e.detail.value,
+  });
+},
+//利润
+handle_inrate(e){
+  console.log("利润:",e.detail.value);
+  this.setData({
+    value_inrate: e.detail.value,
+  })
+},
+//参考平方价
+handle_price(e){
+
+},
+//计算平方价
+calc(){
+  const reg = this.data.value_matcde.search('^[0-9a-zA-Z]+');
+  if(reg == -1) {
+     dd.arlt({content: '配方格式不对'});
+     return;
+  }
+
+ this.calcPrice(); //参考平方价
+  
+},
+//异步计算平方价
+async calcPrice(json){
+
+    //调用配方计算接口
+  const res1 = await dd.httpRequest({
+    headers: {
+      "Content-Type": "application/json"
+    },
+    url: 'http://127.0.0.1:5232/api/Papers/Calc',
+    method: 'POST',
+
+    // 需要手动调用JSON.stringify将数据进行序列化
+      data: JSON.stringify({
+        matcde: this.data.value_matcde,
+        crrcde: this.data.crrcdeArray[this.data.crrIndex].name,
+        loss: this.data.value_lssrat,
+        processCost: this.data.value_strprc,
+        profit: this.data.value_inrate
+      }),
+    dataType: 'json',
+
+    success: function(res) {
+      
+      console.log({content: '计算成功'});
+              
+          
+      
+    },
+    fail: function(res) {
+      dd.alert({content: JSON.stringify(res)});
+    },
+    complete: function(res) {
+   
+    }
+
+  });
+
+  console.log(JSON.stringify(res1.data.price));
+ 
+  this.setData({
+    value_price: res1.data.price,
+  });
+
+},
+//
+
+
 
 });
